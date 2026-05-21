f4259a8197d4e6e8fa41b2d57462cca3c5f5495e
wslider <wslider2000@gmail.com>
2026-05-21 13:35:27 -0400

    Q3 Q4 Q5 Q6 in SQL_basics.sql

--- Week1/AssignmentFiles/SQL_basics.sql
+++ Week1/AssignmentFiles/SQL_basics.sql
@@ -13,17 +13,27 @@ order by price desc;
 select * from customers
 where city = 'Lihue';
 
--- Q3) Return the first 5 orders by earliest order_datetime (order_id, order_datetime).
-
+-- Q3) Return the first 5 orders by earliest order_datetime (order_id, order_datetime)
+select order_id, order_datetime from orders
+order by order_datetime asc 
+limit 5;
 
 -- Q4) Find all products with the word 'Latte' in the name.
+select * from products
+where name like '%Latte%';
 
 -- Q5) Show distinct payment methods used in the dataset.
+select distinct payment_method from orders; 
+
 
 -- Q6) For each store, list its name and city/state (one row per store).
+select distinct name, city, state from stores;
+
 
 -- Q7) From orders, show order_id, status, and a computed column total_items
 --     that counts how many items are in each order.
+
+
 
 -- Q8) Show orders placed on '2025-09-04' (any time that day).
 
